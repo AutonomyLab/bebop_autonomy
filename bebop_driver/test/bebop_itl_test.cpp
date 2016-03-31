55aa7a3459a7ef324e083050b8dfaa3f1aed3bc6
Mani Monajjemi <mani.monajjemi@gmail.com>
2016-03-31 13:35:06 -0700

    Update the tests for testing odom, gps and TF
    - All tests pass using Bebop 1.0 (2.0.57) and Bebop 2.0 (3.1.0)

--- bebop_driver/test/bebop_itl_test.cpp
+++ bebop_driver/test/bebop_itl_test.cpp
@@ -36,9 +36,12 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSI
 #include <sensor_msgs/Image.h>
 #include <angles/angles.h>
 #include <sensor_msgs/CameraInfo.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 #include <gtest/gtest.h>
-
 
 #include <bebop_msgs/Ardrone3PilotingStateFlyingStateChanged.h>
 #include <bebop_msgs/Ardrone3PilotingStateAttitudeChanged.h>
@@ -193,11 +196,9 @@ protected:
     land_pub_= nh_.advertise<std_msgs::Empty>("land", 1);
     cmdvel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 
-
     // Common Subs
     image_.reset(new util::ASyncSub<sensor_msgs::Image>(
                    nh_, "image_raw", 10));
-
 
     spinner_ptr_.reset(new ros::AsyncSpinner(4));
     spinner_ptr_->start();
@@ -243,6 +244,7 @@ protected:
  *
  * (not ROS compatible)
  *
+ * odom on the other hand is REP-103 compatible
  */
 TEST_F(BebopInTheLoopTest, Piloting)
 {
@@ -268,6 +270,14 @@ TEST_F(BebopInTheLoopTest, Piloting)
       bat_state(new util::ASyncSub<bebop_msgs::CommonCommonStateBatteryStateChanged>(
                       nh_, "states/common/CommonState/BatteryStateChanged", 10));
 
+  boost::shared_ptr<util::ASyncSub<nav_msgs::Odometry> >
+      odom(new util::ASyncSub<nav_msgs::Odometry>(
+             nh_, "odom", 10));
+
+  boost::shared_ptr<util::ASyncSub<sensor_msgs::NavSatFix> >
+      gps(new util::ASyncSub<sensor_msgs::NavSatFix>(
+            nh_, "fix", 10));
+
   ros::Publisher takeoff_pub =  nh_.advertise<std_msgs::Empty>("takeoff", 1);
   ros::Publisher reset_pub = nh_.advertise<std_msgs::Empty>("reset", 1);
 
@@ -287,6 +297,8 @@ TEST_F(BebopInTheLoopTest, Piloting)
   TIMED_ASSERT(5.0, speed_state->IsActive(), "Waiting for Speed measurements ...");
   TIMED_ASSERT(5.0, alt_state->IsActive(), "Waiting for alt measurements ...");
   TIMED_ASSERT(5.0, att_state->IsActive(), "Waiting for attitude measurements ...");
+  TIMED_ASSERT(5.0, odom->IsActive(), "Waiting for odom ...");
+  TIMED_ASSERT(5.0, gps->IsActive(), "Waiting for GPS ...");
 
   // TODO(mani-monaj): Use proper values for pitch/roll test thresholds based on cmd_vel
 
@@ -296,14 +308,24 @@ TEST_F(BebopInTheLoopTest, Piloting)
   tw.angular.z = 0.0;
   ROS_WARN("Moving forwared ...");
   cmdvel_pub_.publish(tw);
-  TIMED_ASSERT(2.0, angles::to_degrees(att_state->GetMsgCopy().pitch) < -5.0 , "Measuring pitch ...");
+
+  TIMED_ASSERT(3.0, angles::to_degrees(att_state->GetMsgCopy().pitch) < -2.5 , "Measuring pitch ...");
+  TIMED_ASSERT(3.0, (odom->GetMsgCopy().twist.twist.linear.x > 0.2),
+               "Measuring the forward and lateral velocities from odom ...");
 
   StopBebop();
   TIMED_ASSERT(5.0,
                (fabs(speed_state->GetMsgCopy().speedX) < 0.05) &&
                (fabs(speed_state->GetMsgCopy().speedY) < 0.05) &&
                (fabs(speed_state->GetMsgCopy().speedZ) < 0.05)
-               , "Measuring speed vector ...");
+               , "Measuring the speed vector ...");
+
+  TIMED_ASSERT(5.0,
+               (fabs(odom->GetMsgCopy().twist.twist.linear.x) < 0.05) &&
+               (fabs(odom->GetMsgCopy().twist.twist.linear.y) < 0.05) &&
+               (fabs(odom->GetMsgCopy().twist.twist.linear.z) < 0.05)
+               , "Measuring the speed vector from odom...");
+
 
   tw.linear.x = -0.4;
   tw.linear.y = 0.0;
@@ -311,8 +333,10 @@ TEST_F(BebopInTheLoopTest, Piloting)
   tw.angular.z = 0.0;
   ROS_WARN("Moving Backward ...");
   cmdvel_pub_.publish(tw);
-  TIMED_ASSERT(2.0, angles::to_degrees(att_state->GetMsgCopy().pitch) > 5.0, "Measuring pitch ...");
 
+  TIMED_ASSERT(3.0, angles::to_degrees(att_state->GetMsgCopy().pitch) > 2.5, "Measuring pitch ...");
+  TIMED_ASSERT(3.0, (odom->GetMsgCopy().twist.twist.linear.x < -0.2),
+               "Measuring the forward and lateral velocities from odom ...");
   StopBebop();
 
   tw.linear.x = 0.0;
@@ -321,7 +345,9 @@ TEST_F(BebopInTheLoopTest, Piloting)
   tw.angular.z = 0.0;
   ROS_WARN("Moving left ...");
   cmdvel_pub_.publish(tw);
-  TIMED_ASSERT(2.0, angles::to_degrees(att_state->GetMsgCopy().roll) < -5.0, "Measuring roll ...");
+  TIMED_ASSERT(3.0, angles::to_degrees(att_state->GetMsgCopy().roll) < -2.5, "Measuring roll ...");
+  TIMED_ASSERT(3.0, (odom->GetMsgCopy().twist.twist.linear.y > 0.1),
+               "Measuring the forward and lateral velocities from odom ...");
 
   StopBebop();
 
@@ -331,14 +357,18 @@ TEST_F(BebopInTheLoopTest, Piloting)
   tw.angular.z = 0.0;
   ROS_WARN("Moving right ...");
   cmdvel_pub_.publish(tw);
-  TIMED_ASSERT(2.0, angles::to_degrees(att_state->GetMsgCopy().roll) > 5.0, "Measuring roll ...");
-
+  TIMED_ASSERT(3.0, angles::to_degrees(att_state->GetMsgCopy().roll) > 2.5, "Measuring roll ...");
+  TIMED_ASSERT(3.0, (odom->GetMsgCopy().twist.twist.linear.y < -0.1),
+               "Measuring the forward and lateral velocities from odom ...");
   StopBebop();
 
-  double alt_start, yaw_start;
+  double alt_start, yaw_start, alt_start_odom, yaw_start_odom;
   // Make sure altitude is fresh
   ASSERT_LT(alt_state->GetFreshness().toSec(), 0.5);
+  ASSERT_LT(odom->GetFreshness().toSec(), 0.5);
+
   alt_start = alt_state->GetMsgCopy().altitude;
+  alt_start_odom = odom->GetMsgCopy().pose.pose.position.z;
   tw.linear.x = 0.0;
   tw.linear.y = 0.0;
   tw.linear.z = 0.2;
@@ -347,14 +377,18 @@ TEST_F(BebopInTheLoopTest, Piloting)
   cmdvel_pub_.publish(tw);
   TIMED_ASSERT(10.0,
                ((alt_state->GetMsgCopy().altitude - alt_start) >= 0.5) &&
-               (speed_state->GetMsgCopy().speedZ < -0.05),
-               "Measuring altitude and speed.z...");
+               ((odom->GetMsgCopy().pose.pose.position.z - alt_start_odom) >= 0.5) &&
+               (speed_state->GetMsgCopy().speedZ < -0.05) &&
+               (odom->GetMsgCopy().twist.twist.linear.z > 0.05),
+               "Measuring altitude, speed.z and vertical velocity from odom ...");
 
   StopBebop();
 
   // Make sure altitude is fresh
   ASSERT_LT(alt_state->GetFreshness().toSec(), 0.5);
+  ASSERT_LT(odom->GetFreshness().toSec(), 0.5);
   alt_start = alt_state->GetMsgCopy().altitude;
+  alt_start_odom = odom->GetMsgCopy().pose.pose.position.z;
   tw.linear.x = 0.0;
   tw.linear.y = 0.0;
   tw.linear.z = -0.2;
@@ -363,13 +397,16 @@ TEST_F(BebopInTheLoopTest, Piloting)
   cmdvel_pub_.publish(tw);
   TIMED_ASSERT(10.0,
                ((alt_state->GetMsgCopy().altitude - alt_start) <= -0.5) &&
-               (speed_state->GetMsgCopy().speedZ > 0.05),
-               "Measuring altitude and speed.z ...");
+               ((odom->GetMsgCopy().pose.pose.position.z - alt_start_odom) <= -0.5) &&
+               (speed_state->GetMsgCopy().speedZ > 0.05) &&
+               (odom->GetMsgCopy().twist.twist.linear.z < -0.05),
+               "Measuring altitude, speed.z and vertical velocity from odom ...");
 
   StopBebop();
 
-  // Make sure alttitude is fresh
+  // Make sure the attitude is fresh
   ASSERT_LT(att_state->GetFreshness().toSec(), 0.5);
+  ASSERT_LT(odom->GetFreshness().toSec(), 0.5);
   yaw_start = att_state->GetMsgCopy().yaw;
   tw.linear.x = 0.0;
   tw.linear.y = 0.0;
@@ -377,13 +414,15 @@ TEST_F(BebopInTheLoopTest, Piloting)
   tw.angular.z = -0.5;
   ROS_WARN("Rotating CW for 90 degrees ...");
   cmdvel_pub_.publish(tw);
+
+  // TODO(mani-monaj): Add yaw
   TIMED_ASSERT(10.0,
                angles::normalize_angle(att_state->GetMsgCopy().yaw - yaw_start) >= 0.5 * 3.141596,
                "Measuring Yaw");
 
   StopBebop();
 
-  // Make sure alttitude is fresh
+  // Make sure the attitude is fresh
   ASSERT_LT(att_state->GetFreshness().toSec(), 0.5);
   yaw_start = att_state->GetMsgCopy().yaw;
   tw.linear.x = 0.0;
@@ -429,6 +468,73 @@ TEST_F(BebopInTheLoopTest, Piloting)
 //               );
 
   ASSERT_GE(bat_percent - bat_state->GetMsgCopy().percent, 0);
+}
+
+
+// TODO(mani-monaj): Add value tests
+TEST_F(BebopInTheLoopTest, StaticTFTest)
+{
+  tf2_ros::Buffer tf_buffer;
+  tf2_ros::TransformListener tf_listener(tf_buffer, nh_);
+
+  // We manually set the odom_frame_id to odom_custom in the launch file, so the following should throw
+  ASSERT_THROW(tf_buffer.lookupTransform("odom", "base_link", ros::Time(0), ros::Duration(2.0)), tf2::TransformException);
+  try
+  {
+    geometry_msgs::TransformStamped odom_to_base = tf_buffer.lookupTransform("odom_custom", "base_link", ros::Time(0), ros::Duration(5.0));
+
+//    ASSERT_TRUE((fabs(odom_to_base.transform.translation.x) > 0.0) &&
+//                (fabs(odom_to_base.transform.translation.y) > 0.0) &&
+//                (fabs(odom_to_base.transform.translation.z) > 0.0));
+
+    geometry_msgs::TransformStamped base_to_optical = tf_buffer.lookupTransform(
+          "base_link", "camera_optical", ros::Time(0), ros::Duration(5.0));
+
+//    geometry_msgs::TransformStamped camera_to_optical = tf_buffer.lookupTransform(
+//          "camera_base_link", "camera_optical", ros::Time(0), ros::Duration(5.0));
+
+//    tf2::Transform camera_to_optical_tf;
+//    double r, p, y;
+//    tf2::convert(camera_to_optical.transform, camera_to_optical_tf);
+
+//    tf2::Matrix3x3(camera_to_optical_tf.getRotation()).getRPY(r, p, y);
+//    ASSERT_DOUBLE_EQ(r, 0.0);
+//    ASSERT_DOUBLE_EQ(p, 0.0);
+//    ASSERT_DOUBLE_EQ(y, 0.0);
+//    ASSERT_DOUBLE_EQ(camera_to_optical_tf.getOrigin().length(), 0.0);
+  }
+  catch (const tf2::TransformException& ex)
+  {
+    ASSERT_TRUE(false) << ex.what();
+  }
+}
+
+TEST_F(BebopInTheLoopTest, GPSTest)
+{
+  boost::shared_ptr<util::ASyncSub<sensor_msgs::NavSatFix> > fix_sub(
+        new util::ASyncSub<sensor_msgs::NavSatFix>(nh_, "fix", 10));
+
+  TIMED_ASSERT(2.0, fix_sub->IsActive() , "Waiting for the GPS topic ...");
+
+  sensor_msgs::NavSatFix gps = fix_sub->GetMsgCopy();
+
+  ASSERT_GT(gps.header.frame_id.length(), 0);
+  ASSERT_NE(gps.status.service, 0);
+
+  const bool is_valid = gps.status.status >= sensor_msgs::NavSatStatus::STATUS_FIX;
+
+  if (!is_valid)
+  {
+    ASSERT_DOUBLE_EQ(gps.latitude, 500.0);
+    ASSERT_DOUBLE_EQ(gps.longitude, 500.0);
+    ASSERT_DOUBLE_EQ(gps.altitude, 500.0);
+  }
+  else
+  {
+    ASSERT_NE(gps.latitude, 500.0);
+    ASSERT_NE(gps.longitude, 500.0);
+    ASSERT_NE(gps.altitude, 500.0);
+  }
 }
 
 TEST_F(BebopInTheLoopTest, VisionTest)
@@ -491,6 +597,8 @@ TEST_F(BebopInTheLoopTest, CameraMoveTest)
 
   ASSERT_EQ(camera_state->GetMsgCopy().tilt, 13);
   ASSERT_EQ(camera_state->GetMsgCopy().pan, -16);
+
+  // TODO(mani-monaj): Add TF tests
 }
 
 TEST_F(BebopInTheLoopTest, DISABLED_FlatTrimTest)
