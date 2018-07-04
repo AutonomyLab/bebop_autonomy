1ef25662901414fbd36ca554a98bfe749629f255
Thomas Bamford <thomas.bamford@robotics.utias.utoronto.ca>
2018-07-03 21:22:46 -0400

    Add throttle to joy control of autoflight commands

--- bebop_tools/scripts/autoflight_joy.py
+++ bebop_tools/scripts/autoflight_joy.py
@@ -29,16 +29,29 @@ class AutoFlightJoy:
         rospy.Subscriber('record/joy/start', Empty, self.record_start_callback)
         rospy.Subscriber('record/joy/stop', Empty, self.record_stop_callback)
 
+        self.allow_cmd = rospy.Time.now()
+        self.throttle = rospy.get_param('~throttle', 1.0)  # default 1 s
+
         rospy.loginfo('autoflight: Ready to receive commands from joy.')
 
     def start_callback(self, empty):
         """Retrieve flight plan name and publish to bebop autoflight/start."""
+        if rospy.Time.now() <= self.allow_cmd:
+            return
+        self.allow_cmd = (rospy.Time.now() + 
+                          rospy.Duration.from_sec(self.throttle))
+
         string = rospy.get_param('~autoflight/start/fname',
                                  'flightPlan.mavlink')
         self.pub_start.publish(string)
 
     def home_callback(self, empty):
         """Publish alternating bool to bebop autoflight/navigate_home."""
+        if rospy.Time.now() <= self.allow_cmd:
+            return
+        self.allow_cmd = (rospy.Time.now() + 
+                          rospy.Duration.from_sec(self.throttle))
+
         if self.navigatehome:
             self.pub_home.publish(self.navigatehome)
             self.navigatehome = False
@@ -48,6 +61,11 @@ class AutoFlightJoy:
 
     def usertakeoff_callback(self, empty):
         """Publish alternating bool to bebop user_takeoff."""
+        if rospy.Time.now() <= self.allow_cmd:
+            return
+        self.allow_cmd = (rospy.Time.now() + 
+                          rospy.Duration.from_sec(self.throttle))
+
         if self.usertakeoff:
             self.pub_usertakeoff.publish(self.usertakeoff)
             self.usertakeoff = False
@@ -57,10 +75,20 @@ class AutoFlightJoy:
 
     def record_start_callback(self, empty):
         """Publish true to bebop record."""
+        if rospy.Time.now() <= self.allow_cmd:
+            return
+        self.allow_cmd = (rospy.Time.now() + 
+                          rospy.Duration.from_sec(self.throttle))
+
         self.pub_record.publish(True)
 
     def record_stop_callback(self, empty):
         """Publish false to bebop record."""
+        if rospy.Time.now() <= self.allow_cmd:
+            return
+        self.allow_cmd = (rospy.Time.now() + 
+                          rospy.Duration.from_sec(self.throttle))
+
         self.pub_record.publish(False)
 
 
